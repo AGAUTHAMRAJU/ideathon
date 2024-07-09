581bba9fe7f79aa6576d09a9751541367e97e107
A.GAUTHAM RAJU <22q95a6703@mrce.in>
2024-07-09 02:24:32 -0700

    Add files via upload

---
+++ fcm_project/fcmapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+from .forms import ProductForm
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'fcmapp/product_list.html', {'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'fcmapp/product_detail.html', {'product': product})
+
+def product_create(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')  # Redirect to product list after creation
+    else:
+        form = ProductForm()
+    return render(request, 'fcmapp/product_form.html', {'form': form})
+
+def product_edit(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')  # Redirect to product detail after edit
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'fcmapp/product_form.html', {'form': form})
+
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')  # Redirect to product list after deletion
+    # Handle GET request (usually from confirmation page or modal)
+    return render(request, 'fcmapp/product_confirm_delete.html', {'product': product})
